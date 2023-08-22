c86fd3c08a70eaf449fc878bbbec0d618a0dbcd3
christianfelipecarvalho <christianfc001@gmail.com>
2023-08-22 19:43:58 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  { path:'categoria',component:CategoriaComponent,
+  children:[
+    {path:'', component:CategoriaListarComponent},
+    {path:'listar', component:CategoriaListarComponent},
+    {path:'form',component:CategoriaFormComponent}
+
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
