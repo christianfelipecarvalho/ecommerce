fb53c7a44f455a81fb6570be6f6578c09df9df27
christianfelipecarvalho <christianfc001@gmail.com>
2023-08-22 21:37:33 -0300

    Finalizando vinculação do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
-import { CategoriaComponent } from './categoria/categoria.component';
-import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
-import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,17 @@ import { CategoriaFormComponent } from './categoria/categoria-form/categoria-for
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCQMqYqbGs1rJUIEA6qZZXSK-Iawi3D3_0",
+      authDomain: "ecommerce-cfe32.firebaseapp.com",
+      projectId: "ecommerce-cfe32",
+      storageBucket: "ecommerce-cfe32.appspot.com",
+      messagingSenderId: "988234780211",
+      appId: "1:988234780211:web:17d112e61cb56b92921d46",
+      measurementId: "G-N40NE4GSD7"
+    }),
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
