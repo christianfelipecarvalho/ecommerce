b2ec77e7b1be5d6d1893550b21ed94d2883fd3b1
christianfelipecarvalho <christianfc001@gmail.com>
2023-10-15 23:36:42 -0300

    requisicao service

---
+++ src/app/requisicao.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequisicaoService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  del(_rota:string){
+    return this.http.delete("http://localhost:8080" + _rota);
+  }
+
+  get(_rota:string = '/'){
+    return this.http.get("http://localhost:8080" + _rota);
+  }
+
+  post(formData:any, rota:string = ''){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type' : 'application/json'
+      })
+    };
+    return this.http.post('http://localhost:8080/' + rota,formData,httpOptions);
+  }
+
+  put(formData:any, rota:string = ''){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type' : 'application/json'
+      })
+    };
+    return this.http.put('http://localhost:8080/' + rota,formData,httpOptions);
+  }
+}
